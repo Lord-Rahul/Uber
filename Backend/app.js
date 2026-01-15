e3e14a27fed01a48c6e088ae65a856ca04db2dfa
lord-rahul <rahul.23ucs087.btcs@baddiuniv.ac.in>
2026-01-15 20:08:53 +0530

    Initialize backend with Express, dotenv, and mongoose setup

---
+++ Backend/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req,res)=>{
+    res.send("Jai Bajrang Bali .... JAI SIYA RAM ")
+})
+
+export { app };
